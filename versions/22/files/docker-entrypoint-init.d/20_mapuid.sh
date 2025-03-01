c111809529d713536a4d25a26bc8a3de7e08ce6d
Rp70 <lequocdung99vn@yahoo.com>
2025-03-01 22:09:59 +0000

    feat: add Node v22

---
+++ versions/22/files/docker-entrypoint-init.d/20_mapuid.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Change uid and gid of node to match current dir's owner
+
+set -e
+
+MAP_WWW_UID=${MAP_WWW_UID:='no'}
+if [ "$MAP_WWW_UID" != "no" ]; then
+    if [ ! -d "$MAP_WWW_UID" ]; then
+        MAP_WWW_UID=$PWD
+    fi
+
+    uid=$(stat -c '%u' "$MAP_WWW_UID")
+    gid=$(stat -c '%g' "$MAP_WWW_UID")
+
+    usermod -u $uid node 2> /dev/null && {
+      groupmod -g $gid node 2> /dev/null || usermod -a -G $gid node
+    }
+    chown -cR node.node /home/node
+fi
